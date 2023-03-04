098d34848db1cf5f55578f5e59f56afb3aabadc2
ergowri <125457855+ergowri@users.noreply.github.com>
2023-03-04 16:17:55 +0530

    script to open chrome,firefox & edge browsers using user input

---
+++ Selenium/src/test/java/browser_actions/launch_browser_window/Launch_Browser_Window_Using_UserInput.java
@@ -0,0 +1,32 @@
+package browser_actions.launch_browser_window;
+
+import java.util.Scanner;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class Launch_Browser_Window_Using_UserInput {
+
+	static WebDriver driver;
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter browser name: ");
+		String browser_name = scanner.next();
+
+		if (browser_name.equals("chrome")) {
+			driver = new ChromeDriver();
+
+		} else if (browser_name.equals("firefox")) {
+			driver = new EdgeDriver();
+
+		} else if (browser_name.equals("edge")) {
+			System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
+			WebDriver driver = new ChromeDriver();
+		}
+
+	}
+
+}
