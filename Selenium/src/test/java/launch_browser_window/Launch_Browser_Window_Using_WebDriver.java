2ec737b094bc144bf9d278de4d82dcead3e13d89
ergowri <125457855+ergowri@users.noreply.github.com>
2023-03-05 12:19:49 +0530

    script to launch browser window using WebDriver driver= new ChromeDriver();

---
+++ Selenium/src/test/java/launch_browser_window/Launch_Browser_Window_Using_WebDriver.java
@@ -0,0 +1,16 @@
+package launch_browser_window;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Launch_Browser_Window_Using_WebDriver {
+
+	public static void main(String[] args) {
+
+		WebDriver driver = new ChromeDriver();
+		driver = new FirefoxDriver();
+
+	}
+
+}
